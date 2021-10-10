cc355628ac7d11b85b9d5347c8e956d9bb874538
LeandroC <leandro.carello.93@gmail.com>
2021-10-10 20:22:41 -0300

    Creando Navbar y logo

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -10,20 +10,12 @@ import {
 export default function Header() {
   return (
     <>
-      {/* <Navbar bg="dark" variant="dark" expand="lg">
-        <Container fluid>
-          <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container fluid>          
+          <Navbar.Brand href="#"><img src='https://i.imgur.com/vVrRswJ.png' className='brand'></img></Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: "100px" }}
-              navbarScroll
-            >
-              <Nav.Link href="#action1">Home</Nav.Link>
-              <Nav.Link href="#action2">Link</Nav.Link>
-            </Nav>
-            <Form className="d-flex">
+          <Form className="d-flex">
               <FormControl
                 type="search"
                 placeholder="Search"
@@ -32,9 +24,17 @@ export default function Header() {
               />
               <Button variant="outline-success">Search</Button>
             </Form>
+            <Nav
+              className="me-auto my-2 my-lg-0 flex-grow-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Nav.Link href="#action1">Home</Nav.Link>
+              <Nav.Link href="#action2">About</Nav.Link>
+            </Nav>            
           </Navbar.Collapse>
         </Container>
-      </Navbar> */}
+      </Navbar>
     </>
   );
 }
