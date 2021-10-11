539da45f9a418cba3e787e91000efe67ca410640
abel-mendez <abel.m.villagra@gmail.com>
2021-10-10 21:35:58 -0300

    agregando logica al search

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import "./App.css";
 import Header from "./components/header/header";
+import { Switch, Route } from "react-router-dom";
 function App() {
   return (
     <>
       <Header />
+      <Switch>
+        <Route exact path="/">
+          <h1>Soy un home</h1>
+        </Route>
+        <Route exact path="/about">
+          <h1>Soy una About</h1>
+        </Route>
+        <Route exact path="/:search">
+          <h1>Buscando con search</h1>
+        </Route>
+        <Route exact path="/food/:nombre">
+          <h1>Soy una Comida</h1>
+        </Route>
+      </Switch>
     </>
   );
 }
