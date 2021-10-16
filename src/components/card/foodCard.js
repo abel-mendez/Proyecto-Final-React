14fe39b2fca75d46f697d70bd741aedfb8cc4d30
LeandroC <leandro.carello.93@gmail.com>
2021-10-15 21:59:46 -0300

    Agregando Food Card y modificando Contact

--- src/components/card/foodCard.js
+++ src/components/card/foodCard.js
@@ -1,15 +1,50 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { getFoodApi } from "../api/getFoodApi";
+import { useParams } from "react-router";
 import Food from "../util/food";
+import Loading from "../util/loading";
 
 export default function FoodCard() {
+  //pido el parametro
+  /*const [foodInfo, setFoodInfo] = useState([]);
+  useEffect(() => {
+    setFoodInfo(getFoodApi.app_id)
+  });*/
+
+  const params = useParams();
+
+  useEffect(() => {
+    getFoodApi.get(params.id).then((data) => console.log(data));
+  }, []);
+
+  console.log(params);
+  // const [buscado, setBuscado] = useState(); //Hook
+  // const buscar = async (params) => {
+
+  //   setBuscado(resp);
+  // };
+
+  // if (buscado === undefined) {
+  //   return (
+  //     <div className="loading">
+  //       <Loading></Loading>
+  //     </div>
+  //   );
+  // }
+
+  //busco en la api
+  //muestro el componente
+  //muestro el loading
+
   return (
     <div>
       <div className="d-flex justify-content-center">
         <Card style={{ width: "20rem" }}>
           <Card.Img variant="top" src="https://i.imgur.com/1iQw2NN.jpg" />
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
+            <Card.Title>asd</Card.Title>
             <Card.Text>Food description</Card.Text>
             <Button variant="primary">Go somewhere</Button>
           </Card.Body>
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,60 +1,68 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function Footer() {
   return (
     <>
       <div className="bg-success fixed-bottom d-flex justify-content-between me-auto my-lg-0 flex-grow-0 p-2">
         <div>
-          Leandro Carello
-          <a
-            target="_blank"
-            rel="noreferrer noopener"
-            className="ms-2"
-            href="https://www.facebook.com/"
-          >
-            <img
-              className="logo"
-              alt="facebook"
-              src="https://raw.githubusercontent.com/abel-mendez/responsive_portafolio/0a8506066c617edae6003310ac0745e47b3c5f50/images/facebook.svg"
-            ></img>
-          </a>
-          <a
-            className="ms-2"
-            rel="noreferrer noopener"
-            href="https://www.instagram.com/"
-          >
-            <img
-              className="logo"
-              alt="instagram"
-              src="https://raw.githubusercontent.com/abel-mendez/responsive_portafolio/0a8506066c617edae6003310ac0745e47b3c5f50/images/instagram.svg"
-            ></img>
-          </a>
-          <a
-            target="_blank"
-            rel="noreferrer noopener"
-            className="ms-2"
-            href="https://github.com/"
-          >
-            <img
-              className="logo"
-              alt="github"
-              src="https://raw.githubusercontent.com/abel-mendez/responsive_portafolio/0a8506066c617edae6003310ac0745e47b3c5f50/images/github.svg"
-            ></img>
-          </a>
-          <a
-            target="_blank"
-            rel="noreferrer noopener"
-            className="ms-2"
-            href="https://twitter.com/"
-          >
-            <img
-              alt="twitter"
-              className="logo"
-              src="https://raw.githubusercontent.com/abel-mendez/responsive_portafolio/0a8506066c617edae6003310ac0745e47b3c5f50/images/twitter.svg"
-            ></img>
-          </a>
+          <div className="bg-primary">Leandro Carello</div>
+          <div>
+            <a
+              target="_blank"
+              rel="noreferrer noopener"
+              className="ms-2"
+              href="https://www.facebook.com/"
+            >
+              <img
+                className="logo"
+                alt="facebook"
+                src="https://raw.githubusercontent.com/abel-mendez/responsive_portafolio/0a8506066c617edae6003310ac0745e47b3c5f50/images/facebook.svg"
+              ></img>
+            </a>
+            <a
+              className="ms-2"
+              rel="noreferrer noopener"
+              href="https://www.instagram.com/"
+            >
+              <img
+                className="logo"
+                alt="instagram"
+                src="https://raw.githubusercontent.com/abel-mendez/responsive_portafolio/0a8506066c617edae6003310ac0745e47b3c5f50/images/instagram.svg"
+              ></img>
+            </a>
+            <a
+              target="_blank"
+              rel="noreferrer noopener"
+              className="ms-2"
+              href="https://github.com/"
+            >
+              <img
+                className="logo"
+                alt="github"
+                src="https://raw.githubusercontent.com/abel-mendez/responsive_portafolio/0a8506066c617edae6003310ac0745e47b3c5f50/images/github.svg"
+              ></img>
+            </a>
+            <a
+              target="_blank"
+              rel="noreferrer noopener"
+              className="ms-2"
+              href="https://twitter.com/"
+            >
+              <img
+                alt="twitter"
+                className="logo"
+                src="https://raw.githubusercontent.com/abel-mendez/responsive_portafolio/0a8506066c617edae6003310ac0745e47b3c5f50/images/twitter.svg"
+              ></img>
+            </a>
+          </div>
         </div>
-        <div>Contact us</div>
+        <div>
+          {" "}
+          <NavLink className=" col-12 col-md-6 col-lg-4 mb-3 " to={`/about/`}>
+            Contact Us
+          </NavLink>
+        </div>
         <div>
           Abel Mendez
           <a
