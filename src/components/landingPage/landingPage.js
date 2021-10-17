85b16aabefd2ca9182aa183b14bb742c881b5964
LeandroC <leandro.carello.93@gmail.com>
2021-10-17 20:15:20 -0300

    Creando Landing Page y modificando links

---
+++ src/components/landingPage/landingPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./landingPage.css";
+
+export default function Landing() {
+  return (
+    <>
+      <div>SOY UNA LANDING PAGE</div>
+      <img
+        src="https://images01.nicepage.com/page/61/81/website-template-preview-61818.jpg"
+        className="cover"
+      ></img>
+    </>
+  );
+}
